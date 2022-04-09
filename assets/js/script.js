e846934fe4a5b845fab64db8ed75889753d59ccc
Denize <denize.f.bassi@gmail.com>
2022-04-09 07:36:10 -0300

    general design changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,9 @@
-var form = document.getElementById("my-form");
+var triggerTabList = [].slice.call(document.querySelectorAll('#minhaAba a'))
+triggerTabList.forEach(function(triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
 
-async function handleSubmit(event) {
-    event.preventDefault();
-    var status = document.getElementById("my-form-status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            status.innerHTML = "Thanks for your submission!";
-            form.reset()
-        } else {
-            response.json().then(data => {
-                if (Object.hasOwn(data, 'errors')) {
-                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-                } else {
-                    status.innerHTML = "Oops! There was a problem submitting your form"
-                }
-            })
-        }
-    }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-    });
-}
-form.addEventListener("submit", handleSubmit)
+    triggerEl.addEventListener('click', function(event) {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+})
